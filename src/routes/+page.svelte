b1e00d468e9855a6c1592cbb63d738f4050429a0
Kai Curry <kai@cape.io>
2023-03-09 19:31:47 +0000

    test deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to Kinderplatz</h1>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Add GraphQL query next and have it render within a page.</p>
