ee006b9d44bb4063b383a9d3e9913310957ae434
Kai Curry <kai@cape.io>
2023-03-24 14:55:12 +0000

    top banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
+  import TopBanner from '$lib/el/top-banner.svelte';
   import Header from '$lib/header/header.svelte';
   import "../app.css";
 </script>
 
+<TopBanner />
 <Header />
 <main>
   <slot></slot>
