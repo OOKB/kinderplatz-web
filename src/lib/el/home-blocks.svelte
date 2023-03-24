b7d7ec94ec439b1de38710aca4f0fe92feb5bbe4
Kai Curry <kai@cape.io>
2023-03-24 16:58:04 +0000

    home blocks

--- src/lib/el/home-blocks.svelte
+++ src/lib/el/home-blocks.svelte
@@ -1,28 +1,36 @@
-<div class="blocks flex">
-  <div class="block bg-orange-500 flex">
+<div class="flex">
+
+  <div class="block bg-orange-500 flex-initial w-64">
     <a href="introduction.html" class="flex darkgray no-style darkgray-hover">
       <p>Learn</p>
     </a>
   </div>
-  <div class="block flex imgBlock">
+
+  <div class="block flex-initial imgBlock w-64">
   </div>
-  <div class="block flex imgBlock">
+
+  <div class="block flex-initial imgBlock w-64">
     <p>Share</p>
   </div>
-  <div class="block bg-white flex">
+
+  <div class="block bg-white flex-initial">
     <p>Grow</p>
   </div>
-  <div class="block bg-green flex">
+
+  <div class="block bg-green flex-initial">
     <a href="/ageGroups/" class="flex white no-style white-hover">
       <p class="white">Create</p>
     </a>
   </div>
-  <div class="block flex imgBlock">
+
+  <div class="block flex-initial imgBlock">
   </div>
-  <div class="block flex">
+
+  <div class="block flex-initial">
     <p>Laugh</p>
   </div>
-  <div class="block bg-purple flex">
+
+  <div class="block bg-purple flex-initial">
     <a href="credentials.html" class="flex white no-style white-hover">
       <p class="white">Love</p>
     </a>
---
+++ src/lib/home/block.svelte
@@ -0,0 +1,18 @@
+<script>
+  import classNames from 'classnames'
+
+  export let link = ''
+  export let color = ''
+  export let word = ''
+  $: classStyle = classNames('block', color, 'flex-initial', 'w-64')
+</script>
+
+<div class={classStyle}>
+  {#if link}
+    <a href="{link}" class="flex darkgray no-style darkgray-hover">
+      <p>{word}</p>
+    </a>
+  {:else}
+    <p>{word}</p>
+  {/if}
+</div>
