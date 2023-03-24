fd53fc51528f430f2716e040b71e8041b4df6d30
Kai Curry <kai@cape.io>
2023-03-24 14:25:11 +0000

    default to loading page by slug

--- src/lib/header/header.svelte
+++ src/lib/header/header.svelte
@@ -3,10 +3,10 @@
     <ul class="flex list-reset">
       <li><a class="orange" href="/introduction">Introduction</a></li>
       <li><a class="yellow" href="/approach/">Our Approach</a></li>
-      <li><a class="green" href="/ageGroups/">Age Groups</a></li>
+      <li><a class="green" href="/agegroups/">Age Groups</a></li>
       <li><a class="blue" href="/tuition/">Tuition</a></li>
-      <li><a class="purple" href="/credentials.html">Credentials</a></li>
-      <li><a class="red" href="/contact.html">Contact Us</a></li>
+      <li><a class="purple" href="/credentials">Credentials</a></li>
+      <li><a class="red" href="/contact">Contact Us</a></li>
     </ul>
   </nav>
 </header>
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import LogoBanner from "$lib/el/logo-banner.svelte";
+  import SvelteMarkdown from 'svelte-markdown'
+
+  /* @type { import('./$houdini').PageData } */
+  export let data
+  $: ({ PageData } = data)
+</script>
+
+<LogoBanner title={$PageData.data.page.title}/>
+
+<aside></aside>
+<article>
+  <SvelteMarkdown source={$PageData.data.page.content} />
+</article>
