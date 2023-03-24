5cb2147a0bc9cc4d496fd54151c5968533e6486b
Kai Curry <kai@cape.io>
2023-03-24 14:31:57 +0000

    home blocks

---
+++ src/lib/el/home-blocks.svelte
@@ -0,0 +1,42 @@
+<div class="blocks flex">
+  <div class="block bg-orange flex">
+    <a href="introduction.html" class="flex darkgray no-style darkgray-hover">
+      <p>Learn</p>
+    </a>
+  </div>
+  <div class="block flex imgBlock">
+  </div>
+  <div class="block flex imgBlock">
+    <p>Share</p>
+  </div>
+  <div class="block bg-white flex">
+    <p>Grow</p>
+  </div>
+  <div class="block bg-green flex">
+    <a href="/ageGroups/" class="flex white no-style white-hover">
+      <p class="white">Create</p>
+    </a>
+  </div>
+  <div class="block flex imgBlock">
+  </div>
+  <div class="block flex">
+    <p>Laugh</p>
+  </div>
+  <div class="block bg-purple flex">
+    <a href="credentials.html" class="flex white no-style white-hover">
+      <p class="white">Love</p>
+    </a>
+  </div>
+  <div class="block flex imgBlock">
+  </div>
+  <div class="block bg-blue flex">
+    <a href="/tuition/" class="flex white no-style white-hover">
+      <p class="white">Smile</p>
+    </a>
+  </div>
+  <div class="block flex imgBlock">
+  </div>
+  <div class="block flex imgBlock">
+    <p>Play</p>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
+<script>
+  import HomeBlocks from "$lib/el/home-blocks.svelte"
+</script>
+
 <h1>Welcome to Kinderplatz</h1>
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Add GraphQL query next and have it render within a page.</p>
+<HomeBlocks />
