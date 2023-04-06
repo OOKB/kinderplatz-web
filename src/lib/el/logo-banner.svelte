3e507a21ef6b8311a379d13e99ddfd5b7d4f2e36
Kristian Bjornard <bjornmeansbear@pm.me>
2023-04-05 23:02:19 -0400

    starting to get the text banner areas on each page figured out

--- src/lib/el/logo-banner.svelte
+++ src/lib/el/logo-banner.svelte
@@ -8,17 +8,14 @@
 
   $: bgColor = getSlugColor($page.params.slug, $page.route.id)
 </script>
-<div class="flex static h-20 {bgColor}">
-  <Logo />
-  <h1 class="title z-0 absolute italic uppercase whitespace-nowrap tracking-tighter text-8xl">{title}</h1>
+<div class="flex min-h-[150px] h-[20vw] relative overflow-hidden {bgColor}">
+  <Logo class="min-w-[200px] max-w-[30%] h-auto z-[1] relative p-[0.5em]"/>
+  <h1 class="text-white absolute text-[18vw] italic font-normal tracking-[-0.08em] z-0 left-[-2vw] whitespace-nowrap uppercase rotate-[0.02turn]">{title}</h1>
 </div>
 
 <!-- Probably convert this to tailwind css? -->
 <style>
-  h1.title {
-    font-weight: 400;
+  h1 {
     font-family: var(--font-stack-serif);
-    left: -1vw;
-    transform: rotate(0.02turn);
   }
 </style>
