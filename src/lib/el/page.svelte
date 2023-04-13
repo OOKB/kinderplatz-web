58011a6672a44eab3ecdd758a8e1abb5cb5c8b4e
Kristian Bjornard <bjornmeansbear@pm.me>
2023-04-13 10:29:13 -0400

    improving the basic page text displays

--- src/lib/el/page.svelte
+++ src/lib/el/page.svelte
@@ -10,8 +10,27 @@
 <div class="flex">
   <slot />
   <!-- how to set a lighter background color that matches section color for the article's BG? -->
-  <article class="flex-[5] mr-8 pt-10 pb-20 px-10 text-natural-600">
-    <SvelteMarkdown source={content} />
-    <slot name="article-bottom" />
+  <article class="flex-[5] mr-8 pt-10 pb-20 px-10">
+    <section>
+      <SvelteMarkdown source={content} />
+    </section>
+    <section>
+      <slot name="article-bottom" />
+    </section>
   </article>
 </div>
+
+<style>
+  article section {
+    max-width: 40em;
+    font-size: 1.25rem;
+    line-height: 1.618;
+  }
+  @media (min-width: 800px) {
+    article section {
+      max-width: 45em;
+      font-size: 1.125rem;
+      line-height: 1.5;
+    }
+  }
+</style>
