7db7db42fce793f61522b4a20e64ab06ca155413
Kristian Bjornard <bjornmeansbear@pm.me>
2023-04-13 10:23:43 -0400

    okay, wrapping all the bottom stuff in a footer tag!

--- src/lib/footer/footer.svelte
+++ src/lib/footer/footer.svelte
@@ -8,12 +8,14 @@
 	import Block from "./block.svelte";
 </script>
 
-<section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 border-y border-red mt-1">
-  <Block><Address /></Block>
-  <Block><Hours /></Block>
-  <Block><Contact /></Block>
-  <Block><Accredited /></Block>
-</section>
+<footer>
+  <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 border-y border-red mt-1">
+    <Block><Address /></Block>
+    <Block><Hours /></Block>
+    <Block><Contact /></Block>
+    <Block><Accredited /></Block>
+  </section>
 
-<Links />
-<Stripe />
+  <Links />
+  <Stripe />
+</footer>
