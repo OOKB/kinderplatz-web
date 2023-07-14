3baf0fdda88c938f5d222658b3eb955b1a573614
Kristian Bjornard <bjornmeansbear@pm.me>
2023-07-14 15:02:20 -0400

    alright! got the colored image overlays working again too.

--- src/lib/aside/aside.svelte
+++ src/lib/aside/aside.svelte
@@ -1,12 +1,16 @@
 <script>
   import _ from 'lodash/fp.js'
-  import { pageColor } from '$lib/stores.js'
   import Link from './link.svelte'
+  import { getContext } from 'svelte';
+  import { pageColor } from '$lib/stores.js'
 
   export let images = []
   export let headings = [] // heading values on this page
   export let pages = [] // pages in this section
   export let links = [] // related links
+
+  let section = getContext('section')
+  $: imgOverlay = `bg-${$section.sectionColor}-100`
 
   function getSrc(image) {
     return image.src.replace('/images/', '/images/w450/')
@@ -21,19 +25,24 @@
     xl:flex-1
   ">
   {#if _.get('[0].src', images)}
-      <div class='
-        image-block
-        relative h-auto w-6/12 order-1 m-0 pb-0
-        md:w-auto md:mr-[-2rem] md:ml-[-2rem] md:mb-[1rem] md:order-[initial]
-        '>
-        <img
-          class="
-            block relative w-full z-[1]
-            border-white border-[0.1rem] border-solid;
-          "
+    <div class='
+      image-block
+      relative h-auto w-6/12 order-1 m-0 pb-0
+      md:w-auto md:mr-[-2rem] md:ml-[-2rem] md:mb-[1rem] md:order-[initial]
+      '>
+      <img
+        class="
+          block relative w-full z-[1]
+          border-white border-[0.1rem] border-solid;
+        "
         src="{getSrc(images[0])}"
         alt="{images[0].alt}"
       />
+      <div class="
+        color-overlay
+        block absolute inset-0 z-[2]
+        {imgOverlay}
+        "></div>
     </div>
   {/if}
   {#if headings && headings.length > 1}
@@ -81,6 +90,11 @@
           src="{getSrc(image)}"
           alt="{image.alt}"
         />
+        <div class="
+          color-overlay
+          block absolute inset-0 z-[2]
+          {imgOverlay}
+          "></div>
       </div>
     {/each}
   {/if}
