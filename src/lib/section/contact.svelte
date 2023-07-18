327d842940a563d61c84b18c287e8351e249f839
Kai Curry <kai@cape.io>
2023-07-18 19:17:36 +0000

    special blocks for contact and credentials pages

---
+++ src/lib/section/contact.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Address from "$lib/el/address.svelte";
+	import ContactBlock from "$lib/el/contact.svelte";
+</script>
+
+<section class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+  <Address />
+  <ContactBlock />
+</section>
---
+++ src/lib/section/credentials.svelte
@@ -0,0 +1,12 @@
+<script>
+  const images = [
+    { url: 'https://mn.gov/dhs/', alt: 'MN DHS Logo', image: '/images/w450/logo/dhs-logo-rgb-color-1200x338-transparent.png' },
+    { url: 'https://www.naeyc.org/', alt: 'NAEYC Logo', image: '/images/w450/logo/naeyc-logo.png' }
+  ]
+</script>
+
+<div class="img-group flex">
+  {#each images as img}
+    <a class="no-style" href={img.url}><img src={img.image} alt={img.alt}></a>
+  {/each}
+</div>
