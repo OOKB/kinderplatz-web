3cbfcf378d4016fc91febf882ddf9b3a40b0f16d
Kristian Bjornard <bjornmeansbear@pm.me>
2023-11-17 10:41:57 -0500

    update urgent

--- src/lib/el/contact.svelte
+++ src/lib/el/contact.svelte
@@ -2,7 +2,7 @@
   <h2 class="text-xl	font-semibold	mb-2">Contact</h2>
   <p>
     Telephone: (612) 725-3481 <br>
-    <em>Urgent: Barb Hamilton (612) 599-9178</em> <br>
+    <em>Urgent: (612) 237-8570</em> <br>
     Fax: (612) 725-3700 <br>
   Email: <a class="darkgray text-darkgray underline hover:text-black" href="mailto:Office.team@kinderplatzkids.com?subject=Website Contact">Office.Team@kinderplatzkids.com</a>
   </p>
